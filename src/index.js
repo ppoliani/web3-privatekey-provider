21a73fa972aa0be9b5facc65e3143db35b1cbcc5
ppoliani <ppoliani@gmail.com>
2020-12-17 12:39:23 +0000

    feat: implement the basic provider logic

--- src/index.js
+++ src/index.js
@@ -5,8 +5,49 @@ const HookedSubprovider = require('@trufflesuite/web3-provider-engine/subprovide
 const ProviderSubprovider = require('@trufflesuite/web3-provider-engine/subproviders/provider')
 const RpcProvider = require('@trufflesuite/web3-provider-engine/subproviders/rpc')
 const WebsocketProvider = require('@trufflesuite/web3-provider-engine/subproviders/websocket')
+const Url = require('url')
 const Transaction = require('ethereumjs-tx')
 
 class PrivateKeyProvider {
+  privKey
+  provider
 
+  constructor(privKey, provider) {
+    this.privKey = privKey
+    this.provider = provider
+    this.#setupEngine()
+  }
+
+  #setupEngine() {
+    const pollingInterval = 4000
+    this.engine = new ProviderEngine({pollingInterval})
+    this.engine.addProvider(new NonceSubProvider())
+    this.engine.addProvider(new FiltersSubprovider())
+  }
+
+  #setupProvider() {
+    if (typeof provider === 'string') {
+      const protocol = (Url.parse(url).protocol || 'http:').toLowerCase()
+
+      switch (protocol) {
+        case 'ws:':
+        case 'wss:':
+          this.engine.addProvider(new WebsocketProvider({rpcUrl: provider}))
+          break
+        default:
+          this.engine.addProvider(new RpcProvider({rpcUrl: provider}))
+      }
+    }
+    else {
+      this.engine.addProvider(new ProviderSubprovider(provider))
+    }
+  }
+
+  send() {
+    return this.engine.send.call(this.engine, payload, callback)
+  }
+
+  sendAsync() {
+    this.engine.sendAsync.call(this.engine, payload, callback)
+  }
 }
