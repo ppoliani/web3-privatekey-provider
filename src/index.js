d0a0e0b3662a0b66f102d23f1fde1cfca5722f13
ppoliani <ppoliani@gmail.com>
2020-12-17 12:25:30 +0000

    chore: setup the project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const ProviderEngine = require('@trufflesuite/web3-provider-engine')
+const FiltersSubprovider = require('@trufflesuite/web3-provider-engine/subproviders/filters')
+const NonceSubProvider = require('@trufflesuite/web3-provider-engine/subproviders/nonce-tracker')
+const HookedSubprovider = require('@trufflesuite/web3-provider-engine/subproviders/hooked-wallet')
+const ProviderSubprovider = require('@trufflesuite/web3-provider-engine/subproviders/provider')
+const RpcProvider = require('@trufflesuite/web3-provider-engine/subproviders/rpc')
+const WebsocketProvider = require('@trufflesuite/web3-provider-engine/subproviders/websocket')
+const Transaction = require('ethereumjs-tx')
+
+class PrivateKeyProvider {
+
+}
